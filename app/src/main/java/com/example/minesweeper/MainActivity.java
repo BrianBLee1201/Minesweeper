a9fdd28128e1685782f942e6449c5c08dd43c1b7
BrianBLee1201 <brianble@usc.edu>
2022-09-10 23:08:25 -0700

    Removed ANYTHING from my friend's. I am sorry for making that mistake. I will not do that ever again.

--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -18,19 +18,18 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     //layout and mines
-    private static final int COLUMN_COUNT = 4;
-    private static final int ROW_COUNT = 4;
-    private static final int MINES_COUNT = 1;
+    private static final int COLUMN_COUNT = 8;
+    private static final int ROW_COUNT = 10;
+    private static final int MINES_COUNT = 4;
     //clock
     private int clock = 0;
     private boolean running = false;
-    //extra variables
-    private HashMap<TextView, int[]> map;
-    private TextView[][] array_map;
-    private HashSet<TextView> mines;
-    private int[][] mines_map; //important for DFS. you stop expanding if you see a number.
-    private HashSet<TextView> visited; //part of DFS
-    private int[][] surroundings = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+
+    //extra variables, very important for minesweeper layout!!
+
+    //determining if winning or losing
+
+    private int total_squares = COLUMN_COUNT * ROW_COUNT;
 
     //this one is important for initializing text upon creation near the mines.
 
@@ -54,11 +53,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cell_tvs = new ArrayList<TextView>();
-        array_map = new TextView[ROW_COUNT][COLUMN_COUNT];
-        mines_map = new int[ROW_COUNT][COLUMN_COUNT];
 
-        // Method (2): add four dynamically created cells
+        //allocation
+
+        cell_tvs = new ArrayList<TextView>();
+
+        // Method (2): add four dynamically created cells. Used in lecture
         androidx.gridlayout.widget.GridLayout grid = (androidx.gridlayout.widget.GridLayout) findViewById(R.id.gridLayout01);
         running = true;
 
@@ -67,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
                 TextView tv = new TextView(this);
                 tv.setHeight( dpToPixel(32) );
                 tv.setWidth( dpToPixel(32) );
-                tv.setTextSize( 16 );//dpToPixel(32) );
+                tv.setTextSize( 16 );
                 tv.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
                 tv.setTextColor(Color.GREEN);
                 tv.setBackgroundColor(Color.GREEN);
@@ -80,15 +80,15 @@ public class MainActivity extends AppCompatActivity {
 
                 grid.addView(tv, lp);
 
+                //extra
+
                 cell_tvs.add(tv);
-                array_map[i][j] = tv;
             }
         }
-        gen_mines();
-        runTimer();
+        runTimer(); //run time, unless a player clicked a mine.
     }
 
-    private void runTimer() {
+    private void runTimer() { //used in lecture; not plagiarized.
         final TextView timeView = (TextView) findViewById(R.id.timer);
         final Handler handler = new Handler();
 
@@ -115,23 +115,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-    //During clicking
-    //setOnClickListener(this::onClickNode);
-
-    public void changetype(View view){
-
-    }
-
-    private int findIndexOfCellTextView(TextView tv) { //helper function
+    private int findIndexOfCellTextView(TextView tv) { //helper function; not plagiarized.
         for (int n=0; n<cell_tvs.size(); n++) {
             if (cell_tvs.get(n) == tv)
                 return n;
         }
         return -1;
     }
+
+    // This part is tricky.
+
+    //During clicking
+    //setOnClickListener(this::onClickNode);??
+
     public void onClickTV(View view){
         TextView tv = (TextView) view;
+
         tv.setTextColor(Color.GRAY);
         tv.setBackgroundColor(Color.LTGRAY);
 
@@ -140,88 +139,5 @@ public class MainActivity extends AppCompatActivity {
         int n = findIndexOfCellTextView(tv);
         int i = n/COLUMN_COUNT;
         int j = n%COLUMN_COUNT;
-
-        if (mines.contains(array_map[i][j])){ //you hit a mine
-            running = false;
-            tv.setTextColor(Color.RED);
-            tv.setBackgroundColor(Color.parseColor("red"));
-        }
-
-        else{
-            int totalmine = mine_surround_helper(i, j);
-            if (totalmine != 0){
-                tv.setText(String.valueOf(totalmine)); //do not expand
-            }
-            else{ //empty square with no mines
-                visited = new HashSet<TextView>();
-                DFS(i, j, view);
-            }
-        }
     }
-
-    //FIGURE OUT WHAT HAPPENS ON THE BOTTOM
-
-    public void gen_mines(){ //randomly generate mines
-        mines = new HashSet<TextView>();
-        Random rand = new Random();
-        for (int i = 0; i < MINES_COUNT; i++){
-            int num1 = rand.nextInt((ROW_COUNT-1));
-            int num2 = rand.nextInt((COLUMN_COUNT-1));
-            if (!mines.isEmpty()){
-                if (mines.contains(array_map[num1][num2])) {
-                    num1 = rand.nextInt((ROW_COUNT-1));
-                    num2 = rand.nextInt((COLUMN_COUNT-1));
-                }
-            }
-            mines.add(array_map[num1][num2]);
-            //debug purposes
-            Log.d("Number Generated",String.valueOf(num1) + " and " + String.valueOf(num2));
-
-        }
-        for (int i = 0; i < MINES_COUNT; i++){
-            for (int j = 0; j < COLUMN_COUNT; j++){
-                if (mines.contains(array_map[i][j])){
-                    mine_surround_helper(i,j);
-                }
-            }
-        }
-    }
-
-    private int mine_surround_helper (int i, int j){
-        int minesme = 0;
-        for (int[] temp: surroundings){
-            int dummyx = i + temp[0];
-            int dummyy = j + temp[1];
-            if ((dummyx >= 0 & dummyy >= 0) & (dummyx < ROW_COUNT & dummyy < COLUMN_COUNT)){
-                if (mines.contains(array_map[dummyx][dummyy])){
-                    minesme++;
-                }
-            }
-        }
-        mines_map[i][j] = minesme;
-        //debug purposes
-        Log.d("Neighbors Generated in " + String.valueOf(i) + " and " + String.valueOf(j), String.valueOf(minesme));
-        return minesme;
-    }
-
-
-
-    public void DFS(int i, int j, View view){
-        //another data structure. Handle bacnkend. You can store number and grab grid number.
-        //GUI. Create another data structure.
-        TextView tv = (TextView) view;
-        if ((i >= 0 && i < ROW_COUNT) && (j >= 0 && j < COLUMN_COUNT)){ //valid range
-            if (!visited.contains(array_map[i][j])){ //expand if we did not discover and is an empty square
-                tv.setTextColor(Color.GRAY);
-                tv.setBackgroundColor(Color.LTGRAY);
-                visited.add(array_map[i][j]);
-                if (mines_map[i][j]==0){
-                    for (int[] temp: surroundings){
-                        DFS((i + temp[0]), (j + temp[1]), view);
-                    }
-                }
-            }
-        }
-    }
-    //settext, gettext.
 }
